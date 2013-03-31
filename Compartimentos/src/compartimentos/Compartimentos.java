44e17c7860d9834644006d55cc9b2b051ac0d2be
Gustavo Rosolem <guspace92@hotmail.com>
2013-03-31 20:48:24 -0300

    Criação do Metodo de Runge Kutta de ordem 4
    Divisão dos metodos em funções
    Runge Kutta de 4 ordem (não tenho certeza se o resultado obtido é o
    correto)
    XLS com a simulação do metodo de EULER para tempo de 0.1, 0.5 e 1.

--- Compartimentos/src/compartimentos/Compartimentos.java
+++ Compartimentos/src/compartimentos/Compartimentos.java
@@ -3,41 +3,83 @@ import javax.swing.JOptionPane;
 import java.io.*;
 
 public class Compartimentos {
-    public static void main(String args[]) {
-        double h = Double.valueOf(JOptionPane.showInputDialog(null,"Digite o passo"));
-        int tam = Integer.valueOf(JOptionPane.showInputDialog(null,"Digite o numero linhas"));
-        double x[] = new double[tam+1];
-        double y[] = new double[tam+1];
-        double yexato[] = new double[tam+1];
-        double erro[] = new double[tam+1];
-        x[0] = 0;
-        y[0] = 1;
+    
+    //Euler
+    public static void euler(double h, int tam, double x[], double y[]) {
         try {
-            FileWriter writer = new FileWriter("resultado.txt");
+            FileWriter writer = new FileWriter("euler.txt");
             PrintWriter saida = new PrintWriter(writer);
             for (int l=1; l <= tam+1; l++) {
                 x[l-1] = x[0] + (l - 1) * h;
             }
             for (int l=1; l <= tam; l++) {
                 y[l] = y[l-1] + h * (-x[l-1]) * y[l-1];
-                yexato[l] = (double)Math.exp(-x[l]*x[l]/2);
-                erro[l] = (y[l] - yexato[l])*100/yexato[l];
+                //yexato[l] = (double)Math.exp(-x[l]*x[l]/2);
+                //erro[l] = (y[l] - yexato[l])*100/yexato[l];
             }
             for (int l=0; l <= tam; l++) {
-                saida.println(//"L = " + l + 
-                x[l] + 
-                "\t" + y[l] + 
-                //"\t" + (-(x[l]*y[l])) + 
-                "\t" + (double)Math.exp(-x[l]*x[l]/2)// + 
-                //"\tErro = " + erro[l]
+                saida.println(
+                    x[l] + 
+                    "\t" + y[l] + 
+                    "\t" + (double)Math.exp(-x[l]*x[l]/2)
                 );
-            }// 0.1 = 0.5 = 1
-            //runge kutta 4
+            }
             saida.close();  
             writer.close();
         } catch(IOException e){
-            //e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Resultado Invalido!");
+            JOptionPane.showMessageDialog(null, "O arquivo destino esta aberto!");
         }
+    }
+    
+    //Runge Kutta de ordem 4
+    public static void rungeKuttaQuatro(double h, int tam, double x[], double y[]) {
+        double k1[] = new double[tam+1];
+        double k2[] = new double[tam+1];
+        double k3[] = new double[tam+1];
+        double k4[] = new double[tam+1];
+        try {
+            FileWriter writer = new FileWriter("rungekutta.txt");
+            PrintWriter saida = new PrintWriter(writer);
+            for (int l=1; l <= tam+1; l++) {
+                x[l-1] = x[0] + (l - 1) * h;
+            }
+            for (int l=1; l <= tam; l++) {
+                y[l] = y[l-1] + h * (-x[l-1]) * y[l-1];
+            }
+            saida.println("L\tX\tY\tK1\tK2\tK3\tK4\tYl+1");
+            for (int l=0; l <= tam; l++) {
+                k1[l] = -x[l] * y[l];
+                k2[l] = -(x[l] + h/2)*(y[l] + h/2 * k1[l]);
+                k3[l] = -(x[l] + h/2)*(y[l] + h/2 * k2[l]);
+                k4[l] = -(x[l] + h)*(y[l] + h * k3[l]);
+                saida.println(
+                    l + 
+                    "\t" + x[l] + 
+                    "\t" + y[l] + 
+                    "\t" + k1[l] + 
+                    "\t" + k2[l] + 
+                    "\t" + k3[l] + 
+                    "\t" + k4[l] + 
+                    "\t" + ((double)y[l] + h/6 * (k1[l] + k2[l] + k3[l] + k4[l]))
+                );
+            }
+            saida.close();  
+            writer.close();
+        } catch(IOException e){
+            JOptionPane.showMessageDialog(null, "O arquivo destino esta aberto!");
+        }
+    }
+    
+    public static void main(String args[]) {
+        double h = Double.valueOf(JOptionPane.showInputDialog(null,"Digite o passo"));
+        int tam = Integer.valueOf(JOptionPane.showInputDialog(null,"Digite o numero linhas"));
+        double x[] = new double[tam+1];
+        double y[] = new double[tam+1];
+        //double yexato[] = new double[tam+1];
+        //double erro[] = new double[tam+1];
+        x[0] = 0;
+        y[0] = 1;
+        euler(h, tam, x, y);
+        rungeKuttaQuatro(h, tam, x, y);
     }
 }
