862407c51c91a0455765be77ddf28a484a37504a
Gustavo Rosolem <guspace92@hotmail.com>
2013-05-26 17:39:04 -0300

    Adicionado exercicio B do DOA

--- Compartimentos/src/compartimentos/SistemaSimples.java
+++ Compartimentos/src/compartimentos/SistemaSimples.java
@@ -10,35 +10,52 @@ package compartimentos;
  */
 public class SistemaSimples {
 
-    public double kUm1(double x1, double x2, double k12, double k21) {
-        return k21 * x2 - k12 * x1;
+    public double kUm1(double x1, double x2, double x3, double k12, double k21, double k31) {
+        return k31 * x3 + k21 * x2 - k12 * x1;
     }
 
-    public double kDois1(double x1, double x2, double k12, double k21, double k1) {
-        return k21 * (x2 + k1) - k12 * (x1 + k1);
+    public double kDois1(double x1, double x2, double x3, double k12, double k21, double k31, double rk1) {
+        return k31 * (x3 + rk1) + k21 * (x2 + rk1) - k12 * (x1 + rk1);
     }
 
-    public double kTres1(double x1, double x2, double k12, double k21, double k2) {
-        return k21 * (x2 + k2) - k12 * (x1 + k2);
+    public double kTres1(double x1, double x2, double x3, double k12, double k21, double k31, double rk2) {
+        return k31 * (x3 + rk2) + k21 * (x2 + rk2) - k12 * (x1 + rk2);
     }
 
-    public double kQuatro1(double x1, double x2, double k12, double k21, double k3) {
-        return k21 * (x2 + k3) - k12 * (x1 + k3);
+    public double kQuatro1(double x1, double x2, double x3, double k12, double k21, double k31, double rk3) {
+        return k31 * (x3 + rk3) + k21 * (x2 + rk3) - k12 * (x1 + rk3);
     }
 
     public double kUm2(double x1, double x2, double k12, double k21, double k20) {
         return k12 * x1 -(k21 + k20) * x2;
     }
 
-    public double kDois2(double x1, double x2, double k12, double k21, double k20, double k1) {
-        return k12 * (x1 + k1) -(k21 + k20) * (x2 + k1);
+    public double kDois2(double x1, double x2, double k12, double k21, double k20, double rk1) {
+        return k12 * (x1 + rk1) -(k21 + k20) * (x2 + rk1);
     }
 
-    public double kTres2(double x1, double x2, double k12, double k21, double k20, double k2) {
-        return k12 * (x1 + k2) -(k21 + k20) * (x2 + k2);
+    public double kTres2(double x1, double x2, double k12, double k21, double k20, double rk2) {
+        return k12 * (x1 + rk2) -(k21 + k20) * (x2 + rk2);
     }
 
-    public double kQuatro2(double x1, double x2, double k12, double k21, double k20, double k3) {
-        return k12 * (x1 + k3) -(k21 + k20) * (x2 + k3);
+    public double kQuatro2(double x1, double x2, double k12, double k21, double k20, double rk3) {
+        return k12 * (x1 + rk3) -(k21 + k20) * (x2 + rk3);
     }
+    
+    public double kUm3(double x3, double k31) {
+        return -k31 * x3;
+    }
+
+    public double kDois3(double x3, double k31, double rk1) {
+        return -k31 * (x3 + rk1);
+    }
+
+    public double kTres3(double x3, double k31, double rk2) {
+        return -k31 * (x3 + rk2);
+    }
+
+    public double kQuatro3(double x3, double k31, double rk3) {
+        return -k31 * (x3 + rk3);
+    }
+    
 }
--- Compartimentos/src/compartimentos/TesteSistemaSimples.java
+++ Compartimentos/src/compartimentos/TesteSistemaSimples.java
@@ -15,12 +15,14 @@ public class TesteSistemaSimples {
 
     public static void main(String args[]) {
         double h = 0.01, t = 0.1;
-        double rk11 = 0, rk12 = 0, rk13 = 0, rk14 = 0;
-        double rk21 = 0, rk22 = 0, rk23 = 0, rk24 = 0;
-        double x1 = 100, x2 = 0;
+        double rk11, rk12, rk13, rk14;
+        double rk21, rk22, rk23, rk24;
+        double rk31, rk32, rk33, rk34;
+        double x1 = 100, x2 = 0, x3 = 20;
         double k12 = 0.03;
         double k21 = 0.02;
         double k20 = 0.01;
+        double k31 = 0.08;
         double auxa = 0, auxc = 0, erro;
         SistemaSimples ss = new SistemaSimples();
         try {
@@ -29,18 +31,23 @@ public class TesteSistemaSimples {
             while (t < 4) {
                 auxa = x2;
                 auxc = x1;
-                rk11 = h * ss.kUm1(x1, x2, k12, k21);
+                rk11 = h * ss.kUm1(x1, x2, x3, k12, k21, k31);
+                rk12 = h * ss.kDois1(x1, x2, x3, k12, k21, k31, rk11);
+                rk13 = h * ss.kTres1(x1, x2, x3, k12, k21, k31, rk12);
+                rk14 = h * ss.kQuatro1(x1, x2, x3, k12, k21, k31, rk13);
                 rk21 = h * ss.kUm2(x1, x2, k12, k21, k20);
-                rk12 = h * ss.kDois1(x1, x2, k12, k21, rk11);
                 rk22 = h * ss.kDois2(x1, x2, k12, k21, k20, rk21);
-                rk13 = h * ss.kTres1(x1, x2, k12, k21, rk12);
                 rk23 = h * ss.kTres2(x1, x2, k12, k21, k20, rk22);
-                rk14 = h * ss.kQuatro1(x1, x2, k12, k21, rk13);
                 rk24 = h * ss.kQuatro2(x1, x2, k12, k21, k20, rk23);
-                System.out.println(t + "\t" + x1 + "\t" + x2);
-                saida.println(t + "\t" + x1 + "\t" + x2);
+                rk31 = h * ss.kUm3(x3, k31);
+                rk32 = h * ss.kDois3(x3, k31, rk31);
+                rk33 = h * ss.kTres3(x3, k31, rk32);
+                rk34 = h * ss.kQuatro3(x3, k31, rk33);
+                System.out.println(t + "\t" + x1 + "\t" + x2 + "\t" + x3);
+                saida.println(t + "\t" + x1 + "\t" + x2 + "\t" + x3);
                 x1 = x1 + (rk11 + 2 * rk12 + 2 * rk13 + rk14) / 6;
                 x2 = x2 + (rk21 + 2 * rk22 + 2 * rk23 + rk24) / 6;
+                x3 = x3 + (rk31 + 2 * rk32 + 2 * rk33 + rk34) / 6;
                 erro = (x2 - auxa) / auxa + (x1 - auxc) / auxc;
                 t = t + h;
             }
